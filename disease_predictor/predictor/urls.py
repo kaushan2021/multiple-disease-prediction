3d9c80e4550cce692718dc589a901e96c6d4dd92
pasindu kaushan <Pasindu.Kaushan@inivossl.com>
2024-01-03 05:35:38 +0000

    input form re design

--- disease_predictor/predictor/urls.py
+++ disease_predictor/predictor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('breast-cancer-prediction/', views.breast_cancer_prediction,name='breast_cancer_prediction'),
     path('', views.home_view,name='home'),
+    path('diagnoser/', views.diagnoser_view,name='diagnoser'),
 ]
--- disease_predictor/predictor/views.py
+++ disease_predictor/predictor/views.py
@@ -6,6 +6,19 @@ from .ml_model import predict_breast_cancer
 def home_view(request):
     return render(request, 'home.html')
 
+def diagnoser_view(request):
+    feature_names_breast_cancer = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+                     'mean smoothness', 'mean compactness', 'mean concavity',
+                     'mean concave points', 'mean symmetry', 'mean fractal dimension',
+                     'radius error', 'texture error', 'perimeter error', 'area error',
+                     'smoothness error', 'compactness error', 'concavity error',
+                     'concave points error', 'symmetry error', 'fractal dimension error',
+                     'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+                     'worst smoothness', 'worst compactness', 'worst concavity',
+                     'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    
+    return render(request, 'diagnoser.html', {'feature_names_breast_cancer': feature_names_breast_cancer})
+
 def breast_cancer_prediction(request):
     context = {}
     if request.method == 'POST':
@@ -15,3 +28,4 @@ def breast_cancer_prediction(request):
         context['prediction'] = 'Malignant' if prediction == 1 else 'Benign'
 
     return render(request, 'breast_cancer_prediction.html', context)
+
